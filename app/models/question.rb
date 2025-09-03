d3235a34247572c53a13699f0640549b495dfca2
Ykuroyy <yykm666@gmail.com>
2025-09-03 15:24:36 +0900

    Fix: RuboCop style for issue #2 (alignment, whitespace, double quotes)

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@
 class Question < ApplicationRecord
     has_many :options, dependent: :destroy
     has_many :answers, dependent: :destroy
-  
+
     validates :content, presence: true, length: { maximum: 2000 }
-  end
-  
+end
